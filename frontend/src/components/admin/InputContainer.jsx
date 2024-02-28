68f139b5740647e50c45bb556fd53285617d0543
unawarexi <unawarexi@gmail.com>
2024-02-28 13:43:04 +0100

    Updated

--- frontend/src/components/admin/InputContainer.jsx
+++ frontend/src/components/admin/InputContainer.jsx
@@ -10,7 +10,7 @@ const InputContainer = ({ onChangeText, placeholder }) => {
   }
   return (
     <input
-     className="w-full h-12 roundd-md outline-none border border-third shadow-md bg-secondary
+     className="w-full h-12 rounded-md outline-none border border-third shadow-md bg-secondary
       px-4 text-lg font-semibold font-sans "
       type="text"
       placeholder={placeholder}
--- frontend/src/containers/admin/AdminNewApps.jsx
+++ frontend/src/containers/admin/AdminNewApps.jsx
@@ -10,39 +10,67 @@ const AdminNewApps = () => {
   const [Download, setDownload] = useState("");
   const [Cover, setCover] = useState("");
   const [Banners, setBanners] = useState([]);
-  const [ShortDescription, setShortDescription] = useState("")
+  const [ShortDescription, setShortDescription] = useState("");
 
   return (
     <div className="w-full flex flex-col items-center justify-start px-2 py-3 gap-2">
-      AdminNewApps
       <InputContainer
         className="w-full flex flex-col items-center justify-start px-4 py-3"
         placeholder="App title"
         onChangeText={(data) => setTitle(data)}
       />
 
+      <div className="w-full flex flex-col items-center justify-center">
+        
+
+
+
+      </div>
       <InputContainer
         className="w-full flex flex-col items-center justify-start px-4 py-3"
         placeholder="Company Name"
         onChangeText={(data) => setCompany(data)}
       />
-
       <InputContainer
         className="w-full flex flex-col items-center justify-start px-4 py-3"
         placeholder="App Icon Url"
         onChangeText={(data) => setAppIcon(data)}
       />
-
       <InputContainer
         className="w-full flex flex-col items-center justify-start px-4 py-3"
         placeholder="App Reviews"
         onChangeText={(data) => setReviews(data)}
       />
-
       <InputContainer
         className="w-full flex flex-col items-center justify-start px-4 py-3"
         placeholder="App Total Reviews"
-        onChangeText={(data) => setReviews(data)}
+        onChangeText={(data) => setTotalReviews(data)}
+      />
+      <InputContainer
+        className="w-full flex flex-col items-center justify-start px-4 py-3"
+        placeholder="App Total downloads"
+        onChangeText={(data) => setDownload(data)}
+      />
+      <InputContainer
+        className="w-full flex flex-col items-center justify-start px-4 py-3"
+        placeholder="cover image URL"
+        onChangeText={(data) => setCover(data)}
+      />
+      <InputContainer
+        className="w-full flex flex-col items-center justify-start px-4 py-3"
+        placeholder="App Short Description"
+        onChangeText={(data) => setShortDescription(data)}
+      />
+      <textarea
+        name=""
+        id=""
+        cols="0"
+        rows="10"
+        value={ShortDescription}
+        onChange={(e) => setShortDescription(e.target.value) }
+        placeholder="Description here"
+        className="w-full roundd-md outline-none border border-third shadow-md bg-secondary
+      px-4 text-lg font-semibold font-sans "
       />
     </div>
   );
