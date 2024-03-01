b9f1b1e0183b6e02feaaa7f1cf86f28729533015
unawarexi <unawarexi@gmail.com>
2024-03-01 12:31:32 +0100

    Export components

--- frontend/src/components/ExpComp.js
+++ frontend/src/components/ExpComp.js
@@ -2,4 +2,7 @@ export {default as AdminHeader} from "./admin/AdminHeader";
 
 // used in adminApps
 export {default as InputContainer} from "./admin/InputContainer"
+export {default as AdminAppListCart} from "./admin/AdminAppListCart"
+
+export {default as MainLoader} from "./loader/MainLoader"
 
