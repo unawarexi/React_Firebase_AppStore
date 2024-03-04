9efd9b47fd8e74e475e8426bd9b68d1902539ddf
unawarexi <unawarexi@gmail.com>
2024-03-04 10:10:38 +0100

    updated

--- frontend/src/containers/ExpContainers.js
+++ frontend/src/containers/ExpContainers.js
@@ -2,3 +2,4 @@
 
 export {default as AdminNewApps} from "./admin/AdminNewApps";
 export {default as ListOfApps} from "./admin/ListOfApps"
+export {default as UserListCard} from "./admin/UserListCard"
