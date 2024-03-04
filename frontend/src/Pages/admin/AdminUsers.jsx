2e974bfade9a10c5f404cae9914b0bb369883b95
unawarexi <unawarexi@gmail.com>
2024-03-04 10:12:54 +0100

    User mapping

--- frontend/src/Pages/admin/AdminUsers.jsx
+++ frontend/src/Pages/admin/AdminUsers.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import getUsers from "../../hooks/user/getUsers";
+import { MainLoader } from "../../components/ExpComp";
+import { UserListCard } from "../../containers/ExpContainers";
 
 const AdminUsers = () => {
+  const { data: getusers, isLoading, isError, refetch } = getUsers();
+  if (isLoading) {
+    return <MainLoader />;
+  }
   return (
-    <div>AdminUsers</div>
-  )
-}
+    <div className="w-full flex flex-wrap items-center justify-evenly gap-4">
+      {getusers && getusers?.length > 0 ? (
+        <React.Fragment>
+          {getusers.map((users) => (
+            <UserListCard key={users?.uid} data={users} />
+          ))}
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <p>No Data</p>
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
-export default AdminUsers
+export default AdminUsers;
