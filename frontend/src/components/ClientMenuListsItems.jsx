6469051043afc64b402e37d7e26a8c3ac745952f
unawarexi <unawarexi@gmail.com>
2024-03-10 00:45:23 +0100

    Framer Animation

--- frontend/src/components/ClientMenuListsItems.jsx
+++ frontend/src/components/ClientMenuListsItems.jsx
@@ -1,12 +1,16 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 import { FaChevronDown, FaChevronLeft } from "react-icons/fa6";
+import { layoutSmooth } from "../animation/Animations";
 
 const ClientMenuListsItems = ({ menuItems, isClose }) => {
   const [isSubMenu, setisSubMenu] = useState(false);
 
   return (
+
     <React.Fragment>
+
+         {/* {main menu items spacing} */}
       <li
         className={`group flex items-center gap-x-4 cursor-pointer p-2 px-3 bg-primary hover:bg-[#282828]
         hover:shadow-lg rounded-md w-full ${
@@ -14,6 +18,7 @@ const ClientMenuListsItems = ({ menuItems, isClose }) => {
         } ${isSubMenu && "bg-[#282828]"}`}
         onClick={() => setisSubMenu(!isSubMenu)}
       >
+         {/* {main menu items icon} */}
         <span
           className={`w-8 h-8 flex items-center justify-center rounded-full bg-third
         group-hover:bg-gradient-to-br group-hover:from-heroPrimary group-hover:to-heroSecodary
@@ -27,6 +32,8 @@ const ClientMenuListsItems = ({ menuItems, isClose }) => {
           }
         </span>
 
+        {/* {main menu items title} */}
+
         <span
           className={`text-textPrimary group-hover:text-textSecondary text-base font-medium
         flex-1 duration-200 ${isClose && "hidden"} ${
@@ -36,6 +43,8 @@ const ClientMenuListsItems = ({ menuItems, isClose }) => {
           {menuItems.title}
         </span>
 
+         {/* {submenu items dropdown} */}
+
         {menuItems.submenu && !isClose && (
           <FaChevronDown
             className={`text-textPrimary duration-200 ${
@@ -44,17 +53,26 @@ const ClientMenuListsItems = ({ menuItems, isClose }) => {
           />
         )}
       </li>
-
+       
+        {/* {submenu items display} */}
       <AnimatePresence>
         {menuItems.submenu && isSubMenu && !isClose && (
-          <motion.ul className="bg-primary mt-2 rounded-md ">
+          <motion.ul {...layoutSmooth} className="bg-primary mt-2 rounded-md ">
             {menuItems.subMenuItems.map((item, index) => (
               <li
                 className={`group flex items-center gap-x-4 cursor-pointer py-3 px-4 w-full`}
               >
-               <span>
-                {<menuItems.Icon className ="text-xl block float-left text-textPrimary group-hover:text-heroPrimary"  />}
-               </span>
+                <span>
+                  {
+                    <menuItems.Icon className="text-xl block float-left text-textPrimary group-hover:text-heroPrimary" />
+                  }
+                </span>
+                <span
+                  className={`text-textPrimary group-hover:text-heroPrimary text-base font-medium flex-1
+               duration-200 ${isClose && "hidden"}`}
+                >
+                  {item.title}
+                </span>
               </li>
             ))}
           </motion.ul>
