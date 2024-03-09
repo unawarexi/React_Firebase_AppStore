fb675c528a8b3e8db2e6a2008834589b4f59c7d2
unawarexi <unawarexi@gmail.com>
2024-03-10 00:45:07 +0100

    Mobile AppStore

--- frontend/src/animation/Animations.js
+++ frontend/src/animation/Animations.js
@@ -9,3 +9,9 @@ export const smoothPopIn = {
   animate: { opacity: 1 },
   exit: { opacity: 0 },
 };
+
+export const layoutSmooth = {
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 },
+};
